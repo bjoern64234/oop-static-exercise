6d613d42a1079a258a9de2dd1f12eeaca397af57
bjoern <b.irion@wtnet.de>
2026-05-05 11:29:44 +0200

    Write functions bodies and run testes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,18 +1,22 @@
 public final class Calculator {
 
     public static int addition(int a, int b) {
-        return 0;
+        return a + b;
     }
 
     public static int subtraction(int a, int b) {
-        return 0;
+        return a - b;
     }
 
     public static int multiplication(int a, int b) {
-        return 0;
+        return a * b;
     }
 
     public static double division(int a, int b) {
-        return 0;
+        if (b == 0) {
+            return 0;
+        }
+
+        return (double) a / b;
     }
 }
